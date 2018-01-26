a8eb8812cf73e5b8efeb9f757b033ca05c336b52
Ahmed R. Awadallah <aawadall@ualberta.ca>
2018-01-26 09:59:29 -0500

    Create network skeleton

---
+++ code/network.py
@@ -0,0 +1,6 @@
+# This module should implement a network using multiple layers from layer.py
+# Future enhancment, to implement hyper-parameter tuning mechanism to find an optimal network setup 
+class Network():
+    """Neural Network implementation"""
+    def __init__(self, X, y):
+        """Initialize a network given at least input and output shapes"""
