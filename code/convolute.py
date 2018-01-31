8e8afdf2a9a33b4322fd0bd2449dd9bbb992c472
Ahmed R. Awadallah <aawadall@ualberta.ca>
2018-01-31 17:42:31 -0500

    Create convolute.py

---
+++ code/convolute.py
@@ -0,0 +1,7 @@
+# TODO: This module is to define one or two functions required by a Convoluted Neural Network
+# Final product expects an array of stacked images, 
+# in addition to some tuning parameters such as image size, patch size, pad size and stried
+# output is three dimensional matrix such that Each image is sliced into patches as defined above
+# and each dimension represents one of the following:
+# image instance, patch, pixel in patch 
+# This output will be used later on as an input to another layer that will take one output per patch as a separate feature for the smae instance 
